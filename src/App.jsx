95cd25c65da9f863ba4e0b6b3b21c242c5cf814d
lunaabboud <160365172+lunaabboud@users.noreply.github.com>
2025-10-23 11:17:51 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ThreatIntelPipeline from './components/ThreatIntelPipeline';
+
+function App() {
+  return (
+    <div>
+      <ThreatIntelPipeline />
+    </div>
+  );
+}
+
+export default App;
